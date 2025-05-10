25df96043060cfed8006ed28b58f9ca94a8138ba
jsuisptdr <ayaaya.tk78@gmail.com>
2025-05-10 10:33:53 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,16 +1,12 @@
- #ifndef UTILS_H
- #define UTILS_H
- 
- #include <stdbool.h>
- 
- int readInt(int min, int max, const char *promptMessage, const char *errorMessage);
- 
- char* readString(char *buffer, int size, const char *promptMessage);
- 
- void pauseWithMessage(const char *message);
- 
- int getRandomNumber(int min, int max);
- 
- void initRandom();
- 
- #endif 
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdbool.h>
+
+int readInt(int min, int max, const char *promptMessage, const char *errorMessage);
+char* readString(char *buffer, int size, const char *promptMessage);
+void pauseWithMessage(const char *message);
+int getRandomNumber(int min, int max);
+void initRandom();
+
+#endif // UTILS_H
