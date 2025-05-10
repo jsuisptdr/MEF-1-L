5c996d3daa9239f48ceff892a977d8ecee410f6c
jsuisptdr <ayaaya.tk78@gmail.com>
2025-05-10 10:32:55 +0200

    Update save.h

--- save.h
+++ save.h
@@ -1,12 +1,10 @@
- #ifndef SAVE_H
- #define SAVE_H
- 
- #include "game.h"
- 
- bool saveGame(GameState *game, const char *filename);
- 
- bool loadGame(GameState *game, const char *filename);
- 
- char* listSavedGames();
- 
- #endif 
+#ifndef SAVE_H
+#define SAVE_H
+
+#include "game.h"
+
+bool saveGame(GameState *game, const char *filename);
+bool loadGame(GameState *game, const char *filename);
+char* listSavedGames();
+
+#endif // SAVE_H
